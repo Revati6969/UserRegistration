6f6c175fb15dbac5c8a38742a80df38f65b56b8a
Revati Tekale <revitekale1910@gmail.com>
2020-03-03 18:00:33 +0530

    ADDED:Valid username

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,4 +1,15 @@
 #!/bin/bash -x
-
 echo "Welcome to user registration"
+
+echo "Enter user name"
+read name
+
+pat="^[A-Z][a-z]{2,}"
+
+if [[ $name =~ $pat ]]
+then
+   echo "Valid"
+else
+   echo "Not valid"
+fi
 
